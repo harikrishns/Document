227ba913e7aa703e83349f3a1d8e55bc93274804
harikrishns <harikrishna205jai@gmail.com>
2022-04-08 16:24:09 -0700

    file changes

---
+++ Arithmatic/src/Arith/ArithmaticOperations.java
@@ -0,0 +1,27 @@
+package Arith;
+import java.util.Scanner;
+
+public class ArithmaticOperations {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number");
+	int x=sc.nextInt();
+	System.out.println("Enter second number");
+	int y=sc.nextInt();
+	//perform arithmetic operations.
+	int sum             =x+y;
+	int difference      =x-y;
+	int product         =x*y;
+	int quotient        =x/y;
+	int modulo          =x%y;
+	//print result to console.
+	System.out.println("sum       :"    +sum);
+	System.out.println("difference:"    +difference);
+	System.out.println("product   :"    +product     );
+	System.out.println("quotient  :"    +quotient);
+	System.out.println("modulo    :"    +modulo);
+
+	}
+
+}
